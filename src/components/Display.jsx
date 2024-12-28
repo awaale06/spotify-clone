52b7fd239fc301c6eed7426261bc120d6dc72eee
awaale06 <yasirabdi91@gmail.com>
2024-12-28 18:21:30 +0300

    created display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Displayhome from './Displayhome'
 
 const Display = () => {
   return (
-    <div className=''>
-      
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#1212121] text-white overflow-auto lg:w-[75%] ml-0'>
+      <Routes>
+        <Route path='/' element={<Displayhome/>}/>
+      </Routes>
     </div>
   )
 }
