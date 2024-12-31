24f9cf28b0f0c33b48b6c7bf37138965cde5545f
awaale06 <yasirabdi91@gmail.com>
2024-12-31 22:03:57 +0300

    created display album component and updated display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Displayhome from './Displayhome'
+import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#1212121] text-white overflow-auto lg:w-[75%] ml-0'>
       <Routes>
         <Route path='/' element={<Displayhome/>}/>
+        <Route path='/album/:id' element={<DisplayAlbum/>}/>
       </Routes>
     </div>
   )
